138d771f8a985ffe30608e458ab371de600a01ab
Aviv <shabtay.aviv@gmail.com>
2026-04-04 01:48:31 +0300

    Fixed few bugs * Added support for unaligned addresses. * Refactored finding the permissions.

--- scripts/kcfg.js
+++ scripts/kcfg.js
@@ -1,72 +1,97 @@
 ﻿"use strict";
 
 const log = (x) => host.diagnostics.debugLog(`${x}\n`);
-const system = x => host.namespace.Debugger.Utility.Control.ExecuteCommand(x);
+const system = (x) => host.namespace.Debugger.Utility.Control.ExecuteCommand(x);
 const asUint64 = (x) => host.evaluateExpression(`(unsigned __int64) 0x${x}`);
-const u64 = x => host.memory.readMemoryValues(x, 1, 8)[0];
+const read64 = (x) => host.memory.readMemoryValues(x, 1, 8)[0];
 
 function myGetSymbol(moduleName, symbolName) {
-    return host.parseInt64(system(`x ${moduleName}!${symbolName}`)[0].split(" ")[0].replace('`', ""), 16);
+  return host.parseInt64(
+    system(`x ${moduleName}!${symbolName}`)[0].split(" ")[0].replace("`", ""),
+    16,
+  );
 }
-
-const CFG_PERMISSIONS = {
-    0x0: "Invalid",
-    0x1: "ValidAligned",
-    0x2: "ValidUnaligned",
-    0x3: "ValidExportSuppressed"
-};
 
 function getCfgBitmapBase() {
-    return u64(myGetSymbol("nt", "guard_icall_bitmap"));
+  return read64(myGetSymbol("nt", "guard_icall_bitmap"));
 }
 
+/**
+    Returns the address of validation bit.
+*/
 function getCfgEntry(address) {
-    const cfgBitmapEntryAddress = 
-        asUint64(address)
-        .bitwiseShiftRight(9)
-        .multiply(8)
-        .add(getCfgBitmapBase());
+  const cfgBitmapEntryAddress = asUint64(address)
+    .bitwiseShiftRight(9)
+    .multiply(8)
+    .add(getCfgBitmapBase());
 
-    return cfgBitmapEntryAddress;
+  return cfgBitmapEntryAddress;
 }
 
 class CfgInfo {
-    constructor(target, cfgBitmapEntry, cfgBitmapValue, cfgPermissions){
-        this.target = target;
-        this.entry = cfgBitmapEntry;
-        this.entryValue = cfgBitmapValue;
-        this.permissions = cfgPermissions;
-    }
-};
+  constructor(target, cfgBitmapEntry, cfgBitmapValue, cfgPermissions) {
+    this.target = target;
+    this.entry = cfgBitmapEntry;
+    this.entryValue = cfgBitmapValue;
+    this.permissions = cfgPermissions;
+  }
+}
 
 /**
-    References:
+    The implementation is based on `nt!_guard_dispatch_icall_no_overrides`.
+    For more info checkout:
     - https://documents.trendmicro.com/assets/wp/exploring-control-flow-guard-in-windows10.pdf
-    - https://i.blackhat.com/BH-USA-25/Presentations/USA-25-McGarr-Out-Of-Control-KCFG-And-KCET.pdf
     - https://dl.acm.org/doi/fullHtml/10.1145/3664476.3670432
 */
 function getCfgPermissions(address) {
-    const addressU64 = asUint64(address);
-    if (!(addressU64.bitwiseAnd(0xF) == 0)) {
-        throw new Error("Not supporing unaligned address");
-    }
+  const addressU64 = asUint64(address);
+  const cfgEntryAddress = getCfgEntry(address);
+  const cfgEntryValue = read64(cfgEntryAddress);
+  const isAlignedTargetAddress = addressU64.bitwiseAnd(0xf).asNumber() === 0; // test al, 0Fh
 
-    const entryIndex = addressU64.bitwiseShiftRight(3).bitwiseAnd(0x3F);
-    const cfgEntryAddress = getCfgEntry(address);
-    const cfgEntryValue = u64(cfgEntryAddress);
+  if (isAlignedTargetAddress) {
+    const validBitEntryIndex = addressU64
+      .bitwiseShiftRight(3)
+      .bitwiseAnd(0x3f)
+      .asNumber();
+    const isValid = cfgEntryValue
+      .bitwiseShiftRight(validBitEntryIndex)
+      .bitwiseAnd(1)
+      .asNumber();
+    const permission = isValid === 1 ? "ValidAligned" : "Invalid";
+    return new CfgInfo(addressU64, cfgEntryAddress, cfgEntryValue, permission);
+  }
 
-    const permissions = CFG_PERMISSIONS[cfgEntryValue.bitwiseShiftRight(entryIndex).bitwiseAnd(1).asNumber()];
-    return new CfgInfo(
-        addressU64,
-        cfgEntryAddress,
-        cfgEntryValue,
-        permissions
-    );
+  // unaligned path:
+  const entryPresentIndex = addressU64
+    .bitwiseShiftRight(3)
+    .bitwiseAnd(0x3f)
+    .bitwiseAnd(0x3e)
+    .asNumber(); // btr r10, 0
+
+  // does any valid function start within this 16-byte window?
+  const isEntryPresent = cfgEntryValue
+    .bitwiseShiftRight(entryPresentIndex)
+    .bitwiseAnd(1)
+    .asNumber(); // bt r11, r10
+
+  if (isEntryPresent === 0) {
+    return new CfgInfo(addressU64, cfgEntryAddress, cfgEntryValue, "Invalid");
+  }
+
+  const validBitIndex = entryPresentIndex.bitwiseOr(0x1).asNumber(); // or r10,1
+  const validBit = cfgEntryValue
+    .bitwiseShiftRight(validBitIndex)
+    .bitwiseAnd(1)
+    .asNumber(); // bt r11, r10
+
+  const permission = validBit === 1 ? "ValidUnaligned" : "Invalid";
+  return new CfgInfo(addressU64, cfgEntryAddress, cfgEntryValue, permission);
 }
 
 function initializeScript() {
-    return [
-        new host.apiVersionSupport(1, 9),
-        new host.functionAlias(getCfgPermissions, "kcfg_per"),
-    ];
+  return [
+    new host.apiVersionSupport(1, 9),
+    new host.functionAlias(getCfgPermissions, "kcfg_per"),
+  ];
 }
